f9fc8f13c49328b34782d7ba78a079b867d95911
初志鑫 <1204505056@qq.com>
2020-06-28 14:45:50 +0800

    deploy

---
+++ static/js/chunk-2d230fe7.dde34f23.js
@@ -0,0 +1,6 @@
+/*!
+ *  基于vue-admin-beautiful构建 
+ *  copyright: chuzhixin 1204505056@qq.com 
+ *  time: 2020-6-28 14:45:08
+ */
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d230fe7"],{ef3c:function(e,r,t){"use strict";t.r(r);t("e35a"),t("5e9f");var n,a,u={name:"Redirect",watch:{$route:{handler:function(e){var r=this.$route,t=r.params,n=r.query,a=t.path;this.$router.replace({path:"/"+a,query:n})},immediate:!0}},render:function(e){return e()}},c=u,i=t("9ca4"),o=Object(i["a"])(c,n,a,!1,null,null,null);r["default"]=o.exports}}]);
